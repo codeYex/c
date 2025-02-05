2479be0accfbc91fa799e9aea521578e07dc412d
Code by Yex <yextheprogrammer@gmail.com>
2025-02-05 08:23:15 +0300

    variables

--- appy.c
+++ appy.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    printf("Hello, My name is Richard");
-    return 0;
-}
+// int main(){
+//     printf("Hello, My name is Richard");
+//     return 0;
+// }
+
+// Variables in C
+// int main()
+// {
+//     int age = 34;
+//     float pi = 3.14;
+//     char grade = 'B';
+//     double bigNum = 324.09;
+
+//     printf("Age : %d\n", age);  //%d is used to represent integer
+//     printf("float: %f\n ", pi); // %n is used to represent float
+//     printf("char : %c\n", grade);
+//     printf("double : %f\n", bigNum);
+//     return 0;
+// }
